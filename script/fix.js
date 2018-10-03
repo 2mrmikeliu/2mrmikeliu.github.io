2365358d1c902180e377eaae94fb335c7142402f
2mrmikeliu <mike_server@outlook.com>
2018-10-03 19:44:21 +0800

    fix bugs

--- script/fix.js
+++ script/fix.js
@@ -1,9 +1,13 @@
 function read(data)
 {
+
 var strr="index.top"
-$.get("/markdown/"+strr[0],function(data){
+$.get("/topic/"+strr[0],function(data){
 	var converter = new showdown.Converter();
     var html = converter.makeHtml(data);
- 	document.write("<h1>"+html+"<h1>");}
+ 	document.write("<h1>"+html+"<h1>");
+	document.title(html);
+	}
+	
 );
 }
